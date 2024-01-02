bb8a41dfd52993d11dcea37d6cc0e66f0f78b1d1
Pengfei Ni <feiskyer@gmail.com>
2024-01-02 10:10:23 +0800

    chore: fix the github action

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/feiskyer/kube-copilot
 
-go 1.21.5
+go 1.21
 
 require (
 	github.com/fatih/color v1.16.0
--- pkg/llms/tokens_test.go
+++ pkg/llms/tokens_test.go
@@ -39,9 +39,9 @@ func TestGetTokenLimits(t *testing.T) {
 
 func TestConstrictPrompt(t *testing.T) {
 	type args struct {
-		prompt    string
-		model     string
-		maxTokens int
+		prompt      string
+		model       string
+		tokenLimits int
 	}
 	tests := []struct {
 		name string
@@ -51,34 +51,34 @@ func TestConstrictPrompt(t *testing.T) {
 		{
 			name: "gpt-3.5-turbo-0613",
 			args: args{
-				prompt:    "This is a test prompt.",
-				model:     "gpt-3.5-turbo-0613",
-				maxTokens: 4096,
+				prompt:      "This is a test prompt.",
+				model:       "gpt-3.5-turbo-0613",
+				tokenLimits: 512,
+			},
+			want: "This is a test prompt.",
+		},
+		{
+			name: "gpt-3.5-turbo-0613",
+			args: args{
+				prompt:      "This is a test prompt.",
+				model:       "gpt-3.5-turbo-0613",
+				tokenLimits: 1,
 			},
 			want: "",
 		},
 		{
 			name: "gpt-3.5-turbo-0613",
 			args: args{
-				prompt:    "This is a test prompt.",
-				model:     "gpt-3.5-turbo-0613",
-				maxTokens: 4090,
-			},
-			want: "",
-		},
-		{
-			name: "gpt-3.5-turbo-0613",
-			args: args{
-				prompt:    "This is a test prompt.\nhere is another.",
-				model:     "gpt-3.5-turbo-0613",
-				maxTokens: 4080,
+				prompt:      "This is a test prompt.\nhere is another.",
+				model:       "gpt-3.5-turbo-0613",
+				tokenLimits: 15,
 			},
 			want: "here is another.",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ConstrictPrompt(tt.args.prompt, tt.args.model, tt.args.maxTokens); got != tt.want {
+			if got := ConstrictPrompt(tt.args.prompt, tt.args.model, tt.args.tokenLimits); got != tt.want {
 				t.Errorf("ConstrictPrompt() = %v, want %v", got, tt.want)
 			}
 		})
